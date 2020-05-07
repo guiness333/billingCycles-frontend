5f0b4714ccda6f8d5a430469b75f7a1322ac8fcf
Walber Martins <walber123k@hotmail.com>
2020-05-07 18:35:00 -0300

    Configurações iniciais e rotas principais feitas

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default props=>(
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2020
+            <a href='https://www.github.com/guiness333' target="_blank" > Guiness333</a>
+        </strong>
+    </footer>
+)
