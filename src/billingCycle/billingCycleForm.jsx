443a778c6d7c5d1c07f2d81c2f1a51a4942d4438
Walber Martins <walber123k@hotmail.com>
2020-05-19 14:25:47 -0300

    adicionando o component crediList para mostrar os creditos e debitos dos cyclos de pagamento

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 import labelAndInput from '../common/form/labelAndInput';
 import { init } from './billingCycleActions';
 import { connect } from 'react-redux';
+import CreditList from './creditList';
 
 class BillingCycleForm extends Component{
     render(){
@@ -15,6 +16,7 @@ class BillingCycleForm extends Component{
                     <Field name='name' component={labelAndInput} readOnly={readOnly} label='Nome' cols='12 4' placeholder='Informe o Nome' />
                     <Field name='month' component={labelAndInput} readOnly={readOnly} label='Mês' type='number' cols='12 4' placeholder='Informe o Numero do mês'/>
                     <Field name='year' component={labelAndInput} readOnly={readOnly} label='Ano' cols='12 4' type='number' placeholder='Informe o Ano'/>
+                    <CreditList cols='12 6' />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
