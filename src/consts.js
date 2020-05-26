2752a917f22c3fb562fe73f50d1328434c5e44ae
Walber Martins <walber123k@hotmail.com>
2020-05-26 11:23:16 -0300

    actions de autenticação criados

---
+++ src/consts.js
@@ -0,0 +1,6 @@
+export default {
+    API_URL: 'http://localhost:3003/api',
+    OAPI_URL: 'http://localhost:3003/oapi',
+    TOKEN_VALIDATED: 'TOKEN_VALIDATED',
+    USER_FETCHED: 'USER_FETCHED',
+}
